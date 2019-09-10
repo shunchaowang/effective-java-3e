085d8dd08782e4a379002868ccd249551f336785
Shunchao Wang <wang.2799@osu.edu>
2019-09-10 09:31:24 -0400

    Added Builder pattern.

---
+++ ObjectCreatingDestroying.java
@@ -0,0 +1,67 @@
+/**
+ * Builder pattern
+ */
+class NutritionFacts {
+
+  private final int servingSize;
+  private final int servings;
+  private final int colories;
+  private final int fat;
+  private final int carbohydrate;
+
+  private NutritionFacts(Builder builder) {
+    this.servingSize = builder.servingSize;
+    this.servings = builder.servings;
+
+    this.colories = builder.colories;
+    this.fat = builder.fat;
+    this.carbohydrate = builder.carbohydrate;
+  }
+
+  public String toString() {
+    return getClass() + " - servingSize: " + servingSize + ", servings: " + servings
+        + ", colories: " + colories + ", fat: " + fat + ", carbohydrate: " + carbohydrate;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    NutritionFacts cocaCola =
+        new NutritionFacts.Builder(240, 8).calories(100).carbohydrate(27).build();
+    System.out.println(cocaCola.toString());
+  }
+
+  static class Builder {
+    // Required parameters
+    private final int servingSize;
+    private final int servings;
+
+    // Optional parameters
+    private int colories = 0;
+    private int fat = 0;
+    private int carbohydrate = 0;
+
+    public Builder(int servingSize, int servings) {
+      this.servingSize = servingSize;
+      this.servings = servings;
+    }
+
+    public Builder calories(int val) {
+      colories = val;
+      return this;
+    }
+
+    public Builder fat(int val) {
+      fat = val;
+      return this;
+    }
+
+    public Builder carbohydrate(int val) {
+      carbohydrate = val;
+      return this;
+    }
+
+    public NutritionFacts build() {
+      return new NutritionFacts(this);
+    }
+  }
+}
